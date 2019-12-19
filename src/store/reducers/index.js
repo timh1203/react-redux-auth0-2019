52ba54707c8926f0529a6c1e29074b75f29f6b5e
Timothy H <timh1203@gmail.com>
2019-12-18 22:26:35 -0600

    Add why not redux thunk

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,6 +3,9 @@ import authReducers from './authReducers.js'
 import statusReducers from './statusReducers.js'
 import userReducers from './userReducers.js'
 
+// Why we don't need Redux Thunk
+// https://stackoverflow.com/questions/34570758/why-do-we-need-middleware-for-async-flow-in-redux
+
 const rootReducer = combineReducers({
   authReducers,
   statusReducers,
